5935ea1ea087c38d69175a7e05c76bd4d2c5a8b8
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2024-04-11 19:31:49 +0800

    Example code

--- src/bluiexample/BLUIExample.java
+++ src/bluiexample/BLUIExample.java
@@ -1,6 +1,8 @@
 package bluiexample;
 
+import blui.*;
 import blui.ui.*;
+import mindustry.gen.*;
 import mindustry.mod.*;
 
 public class BLUIExample extends Mod{
@@ -10,6 +12,16 @@ public class BLUIExample extends Mod{
 
     @Override
     public void init(){
-        BLSetup.addTable(t -> t.add("help"));
+        BLSetup.addTable(table -> {
+            table.table(Tex.buttonEdge3, t -> {
+                t.button(Icon.file, BLVars.buttonSize, () -> {});
+                t.button(Icon.planet, BLVars.buttonSize, () -> {});
+                t.row();
+                t.button(Icon.units, BLVars.buttonSize, () -> {});
+                t.button(Icon.discord, BLVars.buttonSize, () -> {});
+                t.button(Icon.distribution, BLVars.buttonSize, () -> {});
+            });
+        });
+        BLSetup.init();
     }
 }
