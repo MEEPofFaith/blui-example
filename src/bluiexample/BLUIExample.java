22aa0256f4ff776223264579e35de99fc696a385
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2024-04-14 21:56:54 -0700

    Match TU's style

--- src/bluiexample/BLUIExample.java
+++ src/bluiexample/BLUIExample.java
@@ -15,22 +15,22 @@ public class BLUIExample extends Mod{
         //A bunch of buttons. Always available.
         BLSetup.addTable(table -> {
             table.table(Tex.buttonEdge3, t -> {
-                t.button(Icon.file, BLVars.buttonSize, () -> {});
-                t.button(Icon.planet, BLVars.buttonSize, () -> {});
+                t.button(Icon.file, BLStyles.bluiImageStyle, BLVars.iconSize, () -> {});
+                t.button(Icon.planet, BLStyles.bluiImageStyle, BLVars.iconSize, () -> {});
                 t.row();
-                t.button(Icon.units, BLVars.buttonSize, () -> {});
-                t.button(Icon.discord, BLVars.buttonSize, () -> {});
-                t.button(Icon.distribution, BLVars.buttonSize, () -> {});
+                t.button(Icon.units, BLStyles.bluiImageStyle, BLVars.iconSize, () -> {});
+                t.button(Icon.discord, BLStyles.bluiImageStyle, BLVars.iconSize, () -> {});
+                t.button(Icon.distribution, BLStyles.bluiImageStyle, BLVars.iconSize, () -> {});
             });
         });
 
         //Another bunch of buttons. Only visible in sandbox.
         BLSetup.addTable(table -> {
             table.table(Tex.pane, t -> {
-                t.button(Icon.distribution, BLVars.buttonSize, () -> {});
-                t.button(Icon.liquid, BLVars.buttonSize, () -> {});
-                t.button(Icon.power, BLVars.buttonSize, () -> {});
-                t.button(Icon.effect, BLVars.buttonSize, () -> {});
+                t.button(Icon.distribution, BLStyles.bluiImageStyle, BLVars.iconSize, () -> {});
+                t.button(Icon.liquid, BLStyles.bluiImageStyle, BLVars.iconSize, () -> {});
+                t.button(Icon.power, BLStyles.bluiImageStyle, BLVars.iconSize, () -> {});
+                t.button(Icon.effect, BLStyles.bluiImageStyle, BLVars.iconSize, () -> {});
             });
         }, () -> Vars.state.rules.infiniteResources);
     }
